14c1086cf0f6de29df1593768f25927e3346c253
Hasturchik <90311115+Hasturchik@users.noreply.github.com>
2024-02-15 19:19:54 +0300

    Update get_new_message.py

--- get_new_message.py
+++ get_new_message.py
@@ -1,41 +1,58 @@
+import time
+
 from bs4 import BeautifulSoup
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import requests
+from datetime import datetime
+import re
 
-
-def get_new_message(driver):
+def get_new_message(driver, date):
     chats = WebDriverWait(driver, 20).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.lhggkp7q.ln8gz9je.rx9719la"))
     )
     messages_list = []
     for chat in chats:
         try:
-            unread = chat.find_element(By.CSS_SELECTOR, "div._2H6nH span")
-            if unread:
-                chat.click()
+            time.sleep(0.5)
+            chat.location_once_scrolled_into_view
+            time.sleep(1)
+            chat.click()
+            # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # WebDriverWait(driver, 20).until(
+            #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.copyable-text")))
+            time.sleep(0.2)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
 
-                html = driver.page_source
+            messages = soup.find_all('div', {'class': 'copyable-text'})
+            # print(soup)
 
-                # Используем BeautifulSoup для парсинга HTML
-                soup = BeautifulSoup(html, 'html.parser')
+            for message in messages:
+                if 'data-pre-plain-text' in message.attrs:
+                    message_date = message['data-pre-plain-text'].split(']')[0][1:]
+                    message_date = datetime.strptime(message_date, "%H:%M, %d.%m.%Y")
 
-                # Находим элемент, после которого начинаются непрочитанные сообщения
-                unread_start = soup.find('div', {'class': '_1Yy5A focusable-list-item'})
+                    if message_date > date:
+                        text = message.find('span', {'class': '_11JPr selectable-text copyable-text'}).get_text()
+                        sender = re.split('] (.+): ', message['data-pre-plain-text'])[-2]
 
-                # Находим все сообщения после плашки о непрочитанных сообщениях
-                messages = unread_start.find_all_next('div', {'class': 'copyable-text'})
+                        # print(text, sender)
 
-                # Извлекаем текст из каждого сообщения
-                for message in messages:
-                    text = message.find('span', {'class': '_11JPr selectable-text copyable-text'}).get_text()
-                    print(text)
-
-                    # messages_list.append({"name": name, "text": text})
+                        message_json = {"text": text, "sender": sender}
+                        messages_list.append(message_json)
+                        print(messages_list)
+                else:
+                    continue
+                    print("Attribute 'data-pre-plain-text' not found in message.")
 
         except NoSuchElementException:
             continue
 
-    print(messages_list)
-    return messages_list, driver
+    response = requests.post('https://yodepro.bpium.ru/api/webrequest/whatsapptest(начать сессию)', json=messages_list)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}, {response.text}")
+
+    # print(messages_list)
